e1f1d731deb81b360c5262fd4141f60e981163a2
Ahmed Hussein <69668781+Ahmed2456@users.noreply.github.com>
2024-07-20 15:59:29 +0300

    Update get_csrf_token.php

--- public/functions/get_csrf_token.php
+++ public/functions/get_csrf_token.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (!isset($_COOKIE['session_id'])) {
+    header("Location: ../login.php");
+    exit();
+}
+
 // Generate CSRF token if not already set
 if (!isset($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32)); // Generate a random token
